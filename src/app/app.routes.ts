4e0c8b1d9dd48558106a4a5adb3b94bbe31b3004
Devfrank94 <francescomurro94@gmail.com>
2025-06-17 00:39:10 +0200

    fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,43 +1,49 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-      path: "",
-      loadComponent: () => import('./pages/home.component'),
-      title: "Home - Games Library",
-    },
-    {
-      path: "platforms",
-      loadComponent: () => import('./pages/platforms.component'),
-      title: "Piattaforme - Games Library",
-    },
-    {
-      path: "creators",
-      loadComponent: () => import('./pages/creators.component'),
-      title: "Creatori - Games Library",
-    },
-    {
-      path: "developers",
-      loadComponent: () => import('./pages/developers.component'),
-      title: "Sviluppatori - Games Library",
-    },
-    {
-      path: "new-releases",
-      loadComponent: () => import('./pages/new-releases.component'),
-      title: "Nuove Uscite - Games Library",
-    },
-    {
-      path: "top-games",
-      loadComponent: () => import('./pages/top-games.component'),
-      title: "Top Giochi - Games Library",
-    },
-    {
-      path: "404",
-      loadComponent: () => import('./pages/error404.component'),
-      title: "Errore 404 Pagina non trovata"
-    },
-    {
-      path: "**",
-      redirectTo: "/404"
-    },
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full"
+  },
+  {
+    path: "home",
+    title: "Home - Games Library",
+    loadComponent: () => import('./pages/home.component'),
+  },
+  {
+    path: "platforms",
+    loadComponent: () => import('./pages/platforms.component'),
+    title: "Piattaforme - Games Library",
+  },
+  {
+    path: "creators",
+    loadComponent: () => import('./pages/creators.component'),
+    title: "Creatori - Games Library",
+  },
+  {
+    path: "developers",
+    loadComponent: () => import('./pages/developers.component'),
+    title: "Sviluppatori - Games Library",
+  },
+  {
+    path: "new-releases",
+    loadComponent: () => import('./pages/new-releases.component'),
+    title: "Nuove Uscite - Games Library",
+  },
+  {
+    path: "top-games",
+    loadComponent: () => import('./pages/top-games.component'),
+    title: "Top Giochi - Games Library",
+  },
+  {
+    path: "404",
+    loadComponent: () => import('./pages/error404.component'),
+    title: "Errore 404 Pagina non trovata"
+  },
+  {
+    path: "**",
+    loadComponent: () => import('./pages/error404.component'),
+    title: "Errore 404 Pagina non trovata"
+  }
 ];
