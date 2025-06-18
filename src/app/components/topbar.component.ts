fd7425f4adb4dd77ed14ac94400e8e1205aef32b
Devfrank94 <francescomurro94@gmail.com>
2025-06-18 03:32:08 +0200

    add topbar component and styling

---
+++ src/app/components/topbar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topbar',
+  imports: [],
+  template: `
+    <div class="bg-base-300 py-2 sm:py-3 shadow-md">
+      <div class="my-container flex items-center justify-center md:justify-start gap-4">
+        <img src="assets/logo.png" alt="logo" class="hidden sm:block sm:w-12 sm:h-12" />
+        <img src="assets/logo-circle.png" alt="logo" class="w-8 h-8 sm:hidden" />
+        <h2 class="hidden sm:block text-2xl font-semibold">Angular Library Games</h2>
+      </div>
+
+    </div>
+  `,
+  styles: ``
+})
+export class TopbarComponent {
+
+}
