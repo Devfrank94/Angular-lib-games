0fd6181be148dfce3fde7bfe276b6eeba3c57368
Devfrank94 <francescomurro94@gmail.com>
2025-06-19 00:05:39 +0200

    Add searchbar component

---
+++ src/app/components/searchbar.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-searchbar',
+  imports: [],
+  template: `
+      <label class="input input-sm rounded-md sm:w-60">
+        <svg class="h-[1.2em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <g
+            stroke-linejoin="round"
+            stroke-linecap="round"
+            stroke-width="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </g>
+        </svg>
+        <input type="search" required placeholder="Cerca..." />
+    </label>
+  `,
+  styles: ``
+})
+export class SearchbarComponent {
+
+}
