3ccedd2ae6aab59516596d488158ed3f4d50c82b
Devfrank94 <francescomurro94@gmail.com>
2025-06-19 00:03:36 +0200

    Add loading component

---
+++ src/app/components/loading.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading',
+  imports: [],
+  template: `
+    <span class="loading loading-infinity loading-xl sm:w-15"></span>
+  `,
+  styles: ``
+})
+export class LoadingComponent {
+
+}
