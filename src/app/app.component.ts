4c68f958a953beb26cb23caeeda84545ddfb45b7
Devfrank94 <francescomurro94@gmail.com>
2025-06-20 19:56:07 +0200

    fix partial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal, effect } from '@angular/core';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { filter, map } from 'rxjs/operators';
+
+// Import componenti
 import { GoToTopComponent } from "./components/go-to-top.component";
 import { FooterComponent } from "./components/footer.component";
 import { NavbarComponent } from "./components/navbar.component";
@@ -8,7 +12,7 @@ import { ToolbarComponent } from "./components/toolbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, GoToTopComponent, FooterComponent, NavbarComponent, TopbarComponent, ToolbarComponent],
+  imports: [CommonModule, RouterOutlet, GoToTopComponent, FooterComponent, NavbarComponent, TopbarComponent, ToolbarComponent],
   template: `
 
     <div class="h-screen flex flex-col">
@@ -17,25 +21,39 @@ import { ToolbarComponent } from "./components/toolbar.component";
         <div class="flex-center-center py-3 sm:py-1">
           <div class="my-container my-box p-3 sm:m-4 sm:p-4 min-h-[70vh] max-h-[80vh]">
             <div class="flex gap-4 py-4 px-1 sm:px-4">
+              <!-- TODO:verifica condizione menu si se 404 non presente -->
+              @if (is404Page()) {
               <div class="hidden sm:block min-w-fit">
                 <!-- Nav desktop -->
-                <app-navbar />
-              </div>
+                  <app-navbar />
+                </div>
+              } @else {
+                <div class="hidden sm:block min-w-fit">
+                <!-- Nav desktop -->
+                  <app-navbar />
+                </div>
+              }
               <div class="w-full flex flex-col gap-4">
                 <app-toolbar />
                 <div class="py-3 sm:py-0 sm:px-1 w-full max-h-[54vh] sm:max-h-[59vh] md:max-h-[65vh] lg:max-h-[60vh] xl:max-h-[65vh] overflow-y-auto rounded-xl">
                   <router-outlet />
                 </div>
-                <!-- Nav mobile -->
+                @if (is404Page()) {
                 <div class="block mt-5 sm:hidden">
+                  <!-- Nav mobile -->
                   <app-navbar />
                 </div>
+                } @else {
+                  <div class="block mt-5 sm:hidden">
+                  <!-- Nav mobile -->
+                  <app-navbar />
+                  </div>
+                }
               </div>
             </div>
           </div>
         </div>
       </div>
-      
       <div>
         <app-go-to-top />
         <app-footer />
@@ -55,5 +73,20 @@ import { ToolbarComponent } from "./components/toolbar.component";
 })
 export class AppComponent {
   title = 'Angular-Games-Library';
+
+  private router = inject(Router);
+  
+  is404Page = signal(false);
+
+  constructor() {
+    // Effect per monitorare i cambiamenti di route
+    effect(() => {
+      this.router.events
+        .pipe(filter(event => event instanceof NavigationEnd))
+        .subscribe((event: NavigationEnd) => {
+          this.is404Page.set(event.url === '/404');
+        });
+    });
+  }
 
 }
