062b149a1d14507fd52472dd8a4804f431a09490
Devfrank94 <francescomurro94@gmail.com>
2025-06-22 20:30:47 +0200

    fix render flash navbar with 404

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, signal, effect, computed } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { filter, map } from 'rxjs/operators';
+import { filter, map, startWith } from 'rxjs/operators';
 import { toSignal } from '@angular/core/rxjs-interop';
 
 // Import componenti
@@ -22,23 +22,31 @@ import { ToolbarComponent } from "./components/toolbar.component";
         <div class="flex-center-center py-3 sm:py-1">
           <div class="my-container my-box p-3 sm:m-4 sm:p-4 min-h-[70vh] max-h-[80vh]">
             <div class="flex gap-4 py-4 px-1 sm:px-4">
-              <!-- TODO:verifica condizione menu si se 404 non presente -->
-              @if (showNavbar()) {
-                <div class="hidden sm:block min-w-fit">
-                  <app-navbar />
-                </div>
+              @switch (navbarState()) {
+                @case ('show') {
+                  <div class="hidden sm:block min-w-fit">
+                    <app-navbar />
+                  </div>
+                }
+                @case ('hide') {}
+                @default {}
               }
               <div class="w-full flex flex-col gap-4">
                 <app-toolbar />
                 <div class="py-3 sm:py-0 sm:px-1 w-full max-h-[54vh] sm:max-h-[59vh] md:max-h-[65vh] lg:max-h-[60vh] xl:max-h-[65vh] overflow-y-auto rounded-xl">
                   <router-outlet />
                 </div>
-                @if (showNavbar()) {
+
+              @switch (navbarState()) {
+                @case ('show') {
                   <div class="block mt-5 sm:hidden">
-                  <!-- Nav mobile -->
-                  <app-navbar />
+                    <!-- Nav mobile -->
+                    <app-navbar />
                   </div>
                 }
+                @case ('hide') {}
+                @default {}
+              }
               </div>
             </div>
           </div>
@@ -61,26 +69,24 @@ import { ToolbarComponent } from "./components/toolbar.component";
     }
 `,
 })
+
 export class AppComponent {
+
   private router = inject(Router);
-  private currentUrl = signal(this.router.url);
-  
-  private navigationEnd = toSignal(
+
+  private currentUrl = toSignal(
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd),
-      map(() => true)
-    )
+      map(() => this.router.routerState.snapshot.url)
+    ),
+    { initialValue: this.router.routerState.snapshot.url }
   );
-  
-  protected showNavbar = computed(() => this.currentUrl() !== '/404');
 
-  constructor() {
-    effect(() => {
-      if (this.navigationEnd()) {
-        setTimeout(() => {
-          this.currentUrl.set(this.router.url);
-        }, 0);
-      }
-    });
-  }
+  protected navbarState = computed(() => {
+    const url = this.currentUrl();
+    if (!url || url === '') return 'loading';
+    return url === '/404' ? 'hide' : 'show';
+  });
+
+
 }
