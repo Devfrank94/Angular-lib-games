667054344846897049929e5493c7c4a83ce3ea9a
Devfrank94 <francescomurro94@gmail.com>
2025-07-28 20:55:39 +0200

    add search games

--- src/app/components/searchbar.component.ts
+++ src/app/components/searchbar.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-searchbar',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   template: `
       <label class="input input-sm rounded-md sm:w-60">
         <svg class="h-[1.2em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -17,11 +20,28 @@ import { Component } from '@angular/core';
             <path d="m21 21-4.3-4.3"></path>
           </g>
         </svg>
-        <input type="search" required placeholder="Cerca..." />
+        <input
+        type="search"
+        placeholder="Cerca..."
+        [(ngModel)]="searchQuery"
+        (input)="onSearch()"
+        (keyup.enter)="onSearch()" />
     </label>
   `,
   styles: ``
 })
 export class SearchbarComponent {
+
+  private readonly apiService = inject(ApiService);
+  searchQuery = signal('');
+
+  onSearch() {
+    const query = this.searchQuery().trim();
+    if (query.length >= 3) {
+      this.apiService.searchGames(query);
+    } else if (query.length === 0) {
+      this.apiService.getGames();
+    }
+  }
 
 }
