b3db6ca6a0b304208d0235ec79469b7a19d571f7
Devfrank94 <francescomurro94@gmail.com>
2025-08-22 19:17:52 +0200

    add conditionally hide searchbar on game detail routes

--- src/app/components/toolbar.component.ts
+++ src/app/components/toolbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { SwitchModeComponent } from "./switch-mode.component";
 import { SearchbarComponent } from "./searchbar.component";
 
@@ -8,7 +10,9 @@ import { SearchbarComponent } from "./searchbar.component";
   template: `
     <div class="p-4 w-full bg-base-300 flex items-center rounded-lg shadow-md gap-3 sm:gap-8">
       <div class="flex-auto">
+        @if (!isGameDetailRoute()) {
         <app-searchbar />
+        }
       </div>
       <div>
         <app-switch-mode />
@@ -18,5 +22,27 @@ import { SearchbarComponent } from "./searchbar.component";
   styles: ``
 })
 export class ToolbarComponent {
+
+  private readonly router = inject(Router);
+  
+  isGameDetailRoute = signal(false);
+
+  constructor() {
+    // Controlla la rotta al caricamento iniziale
+    this.checkRoute();
+    
+    // Ascolta i cambi di navigazione
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.checkRoute();
+      });
+  }
+
+  private checkRoute(): void {
+    const url = this.router.url;
+    const isGameRoute = /^\/game\/\d+\/[^\/]+/.test(url);
+    this.isGameDetailRoute.set(isGameRoute);
+  }
 
 }
